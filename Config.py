78996f586fb482d7fe4495bdc46bc2d4b4e9fd5c
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-02-28 16:17:42 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -13,6 +13,7 @@ class Var(object):
     LOGGER = True
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+    YOUR_NAME = os.environ.get("YOUR_NAME", None)#/
     # Here for later purposes
     SUDO_USERS = set(
         int(x) for x in os.environ.get(
